acca6baedffe4810ef3a90c1f2dfa45b692bd023
da-yoshi-k <hey234234s@gmail.com>
2022-12-08 01:56:16 +0900

    chore: tailwind+daisyUIのインストール

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "app.vue",
+    "./components/**/*.{vue,js,jsx,ts,tsx}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.{vue,js,jsx,ts,tsx}",
+    "./plugins/**/*.{js,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('daisyui')
+  ],
+};
