bfd2768ec68d7d8f83eeacf76578a7e757858dfc
da-yoshi-k <hey234234s@gmail.com>
2022-12-08 21:30:23 +0900

    chore: 一旦のtheme適用

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,21 @@
 module.exports = {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#1EB854",
+          "secondary": "#1FD65F",
+          "accent": "#D99330",
+          "neutral": "#86efac",
+          "base-100": "#f3f4f6",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   content: [
     "app.vue",
     "./components/**/*.{vue,js,jsx,ts,tsx}",
