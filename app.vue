f29d1d7b52b6497942322cbd3d317eee2529569b
da-yoshi-k <hey234234s@gmail.com>
2022-12-16 13:51:49 +0900

    add: 開発環境用の設定を追加

--- app.vue
+++ app.vue
@@ -1,3 +1,12 @@
+<script setup>
+const runTimeConfig = useRuntimeConfig();
+if (runTimeConfig.public.stage === 'development') {
+  useHead({
+    meta: { hid: "robots", name: "robots", content: "noindex" }
+  });
+}
+</script>
+
 <template>
   <div class="flex flex-col min-h-screen">
     <Header />
