a06fd226a00a22bcc81a750ebef334ba48a334fd
da-yoshi-k <hey234234s@gmail.com>
2023-01-07 20:13:55 +0900

    fix: サイドバーとボトムナビゲーションのデザイン・アイコンを修正

--- components/TheBottomNav.vue
+++ components/TheBottomNav.vue
@@ -4,23 +4,31 @@
       <div id="tabs" class="flex justify-between border">
         <NuxtLink to="/home"
           class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
-          <span class="text-2xl"><i class="bx bx-home"></i></span>
-          <span class="tab tab-home block text-md">ホーム</span>
+          <div class="flex justify-center">
+            <img src="/img/nav_home.svg" class="w-6 h-6" />
+          </div>
+          <span class="tab block text-md">ホーム</span>
         </NuxtLink>
         <NuxtLink to="/works"
           class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
-          <span class="text-2xl"><i class="bx bx-user-voice"></i></span>
-          <span class="tab tab-work block text-md">ワーク</span>
+          <div class="flex justify-center">
+            <img src="/img/nav_work.svg" class="w-6 h-6" />
+          </div>
+          <span class="tab block text-md">ワーク</span>
         </NuxtLink>
         <NuxtLink to="/teams"
           class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
-          <span class="text-2xl"><i class="bx bx-group"></i></span>
-          <span class="tab tab-team block text-md">チーム</span>
+          <div class="flex justify-center">
+            <img src="/img/nav_team.svg" class="w-6 h-6" />
+          </div>
+          <span class="tab block text-md">チーム</span>
         </NuxtLink>
         <NuxtLink to="/profile"
           class="w-full focus:text-teal-500 hover:text-teal-500 justify-center inline-block text-center pt-2 pb-1">
-          <span class="text-2xl"><i class="bx bx-user"></i></span>
-          <span class="tab tab-profile block text-xs pt-1">プロフィール</span>
+          <div class="flex justify-center">
+            <img src="/img/nav_profile.svg" class="w-6 h-6" />
+          </div>
+          <span class="tab block text-xs pt-1">プロフィール</span>
         </NuxtLink>
       </div>
     </section>
--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -3,24 +3,29 @@
   <link rel="stylesheet" href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" />
   <div class="hidden md:inline-block">
     <aside class="flex sticky top-16 w-72 flex-col space-y-2 border-r-2 border-gray-200 bg-white p-2"
-      style="height: 88vh">
+      style="height: calc(100vh - 64px)">
       <NuxtLink to="/home" class="flex items-center space-x-1 rounded-md px-5 py-3 hover:bg-gray-300">
-        <span class="text-2xl"><i class="bx bx-home"></i></span>
+        <div class="flex justify-center mr-2">
+          <img src="/img/nav_home.svg" class="w-6 h-6" />
+        </div>
         <span>ホーム</span>
       </NuxtLink>
-
       <NuxtLink to="/works" class="flex items-center space-x-1 rounded-md px-5 py-3 hover:bg-gray-300">
-        <span class="text-2xl"><i class="bx bx-user-voice"></i></span>
+        <div class="flex justify-center mr-2">
+          <img src="/img/nav_work.svg" class="w-6 h-6" />
+        </div>
         <span>ワーク</span>
       </NuxtLink>
-
       <NuxtLink to="/teams" class="flex items-center space-x-1 rounded-md px-5 py-3 hover:bg-gray-300">
-        <span class="text-2xl"><i class="bx bx-group"></i></span>
+        <div class="flex justify-center mr-2">
+          <img src="/img/nav_team.svg" class="w-6 h-6" />
+        </div>
         <span>チーム</span>
       </NuxtLink>
-
       <NuxtLink to="/profile" class="flex items-center space-x-1 rounded-md px-5 py-3 hover:bg-gray-300">
-        <span class="text-2xl"><i class="bx bx-user"></i></span>
+        <div class="flex justify-center mr-2">
+          <img src="/img/nav_profile.svg" class="w-6 h-6" />
+        </div>
         <span>プロフィール</span>
       </NuxtLink>
     </aside>
