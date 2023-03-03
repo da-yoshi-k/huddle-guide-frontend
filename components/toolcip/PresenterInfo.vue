d9e74149bd912a0637eff0f7d630f81598b4f01b
da-yoshi-k <hey234234s@gmail.com>
2023-03-03 23:50:18 +0900

    add: 発表者の説明を追加

---
+++ components/toolcip/PresenterInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <VMenu :delay="{ show: 500, hide: 50 }">
+      <img src='/img/info.svg' />
+
+      <template #popper>
+        <div class="bg-white text-sm p-4 max-w-[320px]">
+          発表順は 自由発表→左上→右上→・・→右下→自由発表 の順に進みます
+        </div>
+      </template>
+    </VMenu>
+  </div>
+</template>
